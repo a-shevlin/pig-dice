94e91d951990a1a0fe8f6c967fb7ca3fa763af87
Seung Lee <shl7@uw.edu>
2022-06-22 14:23:12 -0700

    Pass test on roll

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -28,6 +28,11 @@ describe('Game', () => {
     expect(reusableGame.getActivePlayer()).toEqual(player01);
   });
 
+  test('return true if game has ended', () => {
+    // last turn is 10 so 11 means game has ended
+    expect(reusableGame.endOfGame(11)).toEqual(true);
+  })
+
   test('increments turn and adds currently rolled numbers to total points of the current active player and changes current active player to 2nd argument', () => {
     const player01 = new Player('Jonathan', 0);
     const player02 = new Player('Garrett', 0);
@@ -38,6 +43,23 @@ describe('Game', () => {
     expect(reusableGame.getActivePlayer()).toEqual(player02);
     expect(player01.totalPoints).toEqual(8);
     expect(reusableGame.turnNumber).toEqual(2);
+  });
+
+  // each round, every player gets a turn
+  test('rolls for player object that\'s passed in and passes to next player if 1 is rolled. Returns true if player rolled 1 and round has not ended. Returns false if round has ended', () => {
+    const die = new Die(6);
+    const player01 = new Player('Jonathan', 0);
+    const player02 = new Player('Garrett', 0);
+    reusableGame.addPlayer(player01);
+    reusableGame.addPlayer(player02);
+    const rolledOne = reusableGame.roll(die, player01);
+    if (Number.isInteger(rolledOne)) {
+      expect(rolledOne).toBeLessThan(7);
+      expect(rolledOne).toBeGreaterThan(1);
+    } else if (rolledOne) {
+      expect(reusableGame.turnNumber).toEqual(2);
+      expect(player01.totalPoints).toEqual(0);
+    }
   });
 });
 
