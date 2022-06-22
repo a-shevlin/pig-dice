88839f091f485f80c96068d3ba0645eac000ba4b
Seung Lee <shl7@uw.edu>
2022-06-22 13:23:38 -0700

    Pass test on constructor and addPlayer

---
+++ __tests__/game.test.js
@@ -0,0 +1,54 @@
+import Game from './../src/game.js';
+import Player from './../src/player.js';
+
+describe('Game', () => {
+  let reusableGame;
+
+  beforeEach(() => {
+    reusableGame = new Game(1, 10);
+  });
+
+  test('should create a game object turn starting at 1 and ending at 10', () => {
+    expect(reusableGame.turnNumber).toEqual(1);
+    expect(reusableGame.maxTurnNumber).toEqual(10);
+  });
+
+  test('should be able to add a player', () => {
+    const player = new Player('Jonathan', 0);
+    reusableGame.addPlayer(player);
+    expect(reusableGame.allPlayers[0]).toEqual(player);
+  });
+});
+
+/*
+Description for Game()
+
+Test1: "Create game object that keeps track of active player"
+Code: new Game('Claire')
+Expected Output: { activePlayer: 'Claire' }
+
+Description for Game.prototype.roll
+
+Test1: "Roll a number for playerObj"
+Code: roll(playerObj)
+Expected Output: 
+
+Description for Game.prototype.hold()
+
+Test1: "Switch from current player to next player"
+Code: gameObj.hold(player2, player1);
+Expected Output: Becomes next player's turn
+gameObj {
+  turnNumber: x,
+  maxTurnNumber: y,
+  allPlayers: [],
+  activePlayer: player1
+}
+
+
+Description for Game.prototype.endOfGame()
+
+Test1: 
+Code: endOfGame(currentTurn)
+Expected Output: 
+*/
